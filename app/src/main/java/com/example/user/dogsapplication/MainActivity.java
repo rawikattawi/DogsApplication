085ec67b330e1263cc08969702b34f35a32c8472
user <rawikattawi29@gmail.com>
2019-01-25 08:17:49 +0200

    alert

--- app/src/main/java/com/example/user/dogsapplication/MainActivity.java
+++ app/src/main/java/com/example/user/dogsapplication/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.user.dogsapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DialogInterface.OnClickListener {
     Button btMyDogs, btAboutApp;
 
     @Override
@@ -32,6 +35,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(i);
         }
 
+
+    }
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        if(which==dialog.BUTTON_POSITIVE){
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+
+        
+        if(which==dialog.BUTTON_NEGATIVE) {
+            Toast.makeText(this,"No", Toast.LENGTH_LONG ).show();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("are you sure?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes",this);
+        builder.setNegativeButton("No",this);
+        AlertDialog dialog=builder.create();
+        dialog.show();
 
     }
 }
